6d85de65f39826c62a27f3705161090bb4fd09b2
infinitizon <infinitizon@yahoo.com>
2016-08-31 17:05:38 +0100

    Working on the other pages

--- public/index.php
+++ public/index.php
@@ -5,35 +5,69 @@ $style_css = array('bootstrap-select/1.11.0/bootstrap-select.min.css','ticker.cs
 
 require_once 'assets/common/header.inc';
 ?>
-    <div class="jumbotron cover_img main_img">
-        <div class="container" style="position: relative;">
-            <?php
-            require_once 'assets/common/nav.inc';
-            ?>
-            <div style="margin-top:25%; position:relative;">
-                <form role="form">
-                    <div class="row">
-                        <div class="col-xs-2 text-right">
-                            <div class="form-group">
-                                <select class="selectpicker" data-width="fit">
-                                    <option>Buy</option>
-                                    <option>Rent</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-xs-8">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Search for properties..." id="q">
-                            </div>
-                        </div>
-                        <div class="col-xs-2">
-                            <button type="submit" class="btn btn-danger">Search</button>
-                        </div>
-                    </div>
-                </form>
+<style>
+    /* CUSTOMIZE THE NAVBAR FOR THE INDEX -> http://www.bootply.com/93060
+    -------------------------------------------------- */
+    /* Special class on .container surrounding .navbar, used for positioning it into place. */
+    .navbar-wrapper {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: 10;
+    }
+    .carousel-control.left, .carousel-control.right{
+        background: none !important;
+        filter: progid:none !important;
+    }
+</style>
+<div class="navbar-wrapper">
+    <div class="container">
+        <?php
+        require_once 'assets/common/nav.inc';
+        ?>
+    </div>
+</div>
+<!--  Carousel Begins  -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+<!--   <img src="http://placehold.it/1920x600/02809E/18D18F" class="img-responsive" width="100%" alt="Chania">    -->
+                <img src="/images/carousel/1.jpg" class="img-responsive" width="100%" height="451" alt="Chania">
+            </div>
+
+            <div class="item">
+                <img src="/images/carousel/2.jpg" class="img-responsive" width="100%" height="451"  alt="Chania">
+            </div>
+
+            <div class="item">
+                <img src="/images/bg.jpg" class="img-responsive" width="100%" height="451" alt="Chania">
+            </div>
+
+            <div class="item">
+                <img src="/images/bg.jpg" class="img-responsive" width="100%" height="451" alt="Chania">
             </div>
         </div>
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
+<!--  Carousel Ends  -->
+
     <div class="well-lg notice">
         <div class="container"><h4 style="color:#FFF;">Be the best by discussing with the proffessionals...</h4></div>
     </div>
@@ -67,6 +101,21 @@ require_once 'assets/common/header.inc';
                     <div class="img-circle img-responsive"><img src="images/construction.png" width="70" height="70"  /></div>
                     <h3>Construction Support</h3>
                     <p class="text-justify">Moart is your one stop shop for construction related materials. From providing building materials to supplies of construction equipments. Moart is the A-Z of all your construction needs </p>
+                </article>
+            </div>
+            <hr class="style">
+            <div class="row text-center">
+                <article class="col-sm-3">
+                    <img src="images/estates/new/1.jpg" class="img-thumbnail img-responsive">
+                </article>
+                <article class="col-sm-3">
+                    <img src="images/estates/new/2.jpg" class="img-thumbnail img-responsive" style="width:300px; max-height: 200px;">
+                </article>
+                <article class="col-sm-3">
+                    <img src="images/estates/new/3.jpg" class="img-thumbnail img-responsive">
+                </article>
+                <article class="col-sm-3">
+                    <img src="images/estates/new/4.jpg" class="img-thumbnail img-responsive">
                 </article>
             </div>
         </div>
@@ -120,3 +169,4 @@ require_once 'assets/common/header.inc';
 <?php
 require_once 'assets/common/footer.inc';
 ?>
+
